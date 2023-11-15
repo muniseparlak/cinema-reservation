cc8c5275f576266e41a34139c3a5f40dee3289d7
Munise Parlak <diyetisyenmunise@gmail.com>
2023-11-15 21:55:30 +0300

    rezerve koltuklar için js

--- script.js
+++ script.js
@@ -2,18 +2,27 @@ const container = document.querySelector('.container')
 let money = document.querySelector('.money');
 let number = document.querySelector('.number');
 let seatPrice = document.querySelector('#seatPrice')
+// let payContain = document.querySelector('.pay')
 
 
+// koltukların selected=toggle olması için
 container.addEventListener('click', (e) => {
   if(e.target.classList.contains('chair') && (!e.target.classList.contains('exampleSeat'))){
     e.target.classList.toggle('selected')
     
  // business koltuk seçildiğinde option da prime ı seçmek için
-    if(e.target.classList.contains('business')){
+  if(e.target.classList.contains('business')){
         document.getElementById("prime").selected = "true";
     }
 
+//rezerve koltuk seçildiğinde alert için 
+  if(e.target.classList.contains('reserved')){
+    alert('Bu koltuk rezerve, lütfen başka koltuk seçiniz')
+    !e.target.classList.toggle('selected')
+  }
+
     calculate()
+    showPay()
     
   }
 })
@@ -25,4 +34,8 @@ function calculate() {
     money.innerHTML = selectedChair * price
 
 }
+
+
+
+
 
