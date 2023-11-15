64e2c272f996ca6ac16122386b5b7d51e9e7c9b5
Munise Parlak <diyetisyenmunise@gmail.com>
2023-11-15 23:21:17 +0300

    css düzenlendi

--- script.js
+++ script.js
@@ -1,41 +1,42 @@
-const container = document.querySelector('.container')
-let money = document.querySelector('.money');
-let number = document.querySelector('.number');
-let seatPrice = document.querySelector('#seatPrice')
-// let payContain = document.querySelector('.pay')
-
+const container = document.querySelector(".container");
+let money = document.querySelector(".money"); // parayı ekrana yazdırmak için html seçtik
+let number = document.querySelector(".number"); // kaç koltuk görmek için html kodunu seçtik
+let seatPrice = document.querySelector("#seatPrice"); // select kısmını seçtik
+let payContain = document.querySelector(".pay"); // en altta fiyat ve koltuk sayısının text i
 
 // koltukların selected=toggle olması için
-container.addEventListener('click', (e) => {
-  if(e.target.classList.contains('chair') && (!e.target.classList.contains('exampleSeat'))){
-    e.target.classList.toggle('selected')
-    
- // business koltuk seçildiğinde option da prime ı seçmek için
-  if(e.target.classList.contains('business')){
-        document.getElementById("prime").selected = "true";
+container.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("chair") &&
+    !e.target.classList.contains("exampleSeat")
+  ) {
+    e.target.classList.toggle("selected");
+
+    // business koltuk seçildiğinde option da prime ı seçmek için
+    if (e.target.classList.contains("business")) {
+      document.getElementById("prime").selected = "true";
     }
 
-//rezerve koltuk seçildiğinde alert için 
-  if(e.target.classList.contains('reserved')){
-    alert('Bu koltuk rezerve, lütfen başka koltuk seçiniz')
-    !e.target.classList.toggle('selected')
-  }
+    //rezerve koltuk seçildiğinde alert için
+    if (e.target.classList.contains("reserved")) {
+      alert("Bu koltuk rezerve, lütfen başka koltuk seçiniz");
+      !e.target.classList.toggle("selected");
+    }
 
-    calculate()
-    showPay()
-    
+    calculate();
   }
-})
+});
 
 function calculate() {
-    let price = seatPrice.value;
-    selectedChair = container.querySelectorAll('.chair.selected').length-1
-    number.innerHTML = selectedChair;
-    money.innerHTML = selectedChair * price
+  let price = seatPrice.value; // bilet fiyatı için
+  let selectedChair = container.querySelectorAll(".chair.selected").length - 1; // seçili koltuk sayısı
+  number.innerHTML = selectedChair;
+  money.innerHTML = selectedChair * price;
 
+  if(selectedChair > 0){
+    payContain.style.display = 'block'
+  }else{
+    payContain.style.display = 'none'
+  }
 }
-
-
-
-
 
